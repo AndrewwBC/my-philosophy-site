e20856673274b9aa4ed2a8bfe2b7c60945b051ba
AndrewwBC <andrew-hot2011@hotmail.com>
2025-02-16 00:40:20 -0300

    styles(Post) author and post numbers

--- src/pages/Feed/Posts/Post/index.tsx
+++ src/pages/Feed/Posts/Post/index.tsx
@@ -1,5 +1,17 @@
 import { useEffect, useRef } from "react";
-import { PostContainer, PostInfo } from "./styles";
+import {
+  EditorContainer,
+  Icons,
+  PostContainer,
+  PostInfo,
+  User,
+  UserAndPostNumbers,
+} from "./styles";
+import UsernameAndImg from "../../../../components/UsernameAndImg";
+
+import { PiHandsClapping } from "react-icons/pi";
+import { FaRegComment } from "react-icons/fa";
+import { FaCalendarAlt } from "react-icons/fa";
 
 function Post() {
   const divRef = useRef<HTMLDivElement>(null);
@@ -9,6 +21,20 @@ function Post() {
       '<p><span style="background-color: rgb(230, 0, 0); color: rgb(205, 205, 205);">Platão foi um filósofo e matemático do período clássico da Grécia Antiga, autor de diversos diálogos filosóficos e fundador da Academia em Atenas, a primeira instituição de educação superior do mundo ocidental.</span></p>';
   }, []);
 
+  const iconsData = [
+    {
+      icon: <PiHandsClapping className="ico" size={22} />,
+      value: 234,
+    },
+    {
+      icon: <FaRegComment className="ico" size={18} />,
+      value: 129,
+    },
+    {
+      icon: <FaCalendarAlt className="ico" size={18} />,
+      value: "Set 24, 2024",
+    },
+  ];
   return (
     <PostContainer>
       <PostInfo>
@@ -17,7 +43,27 @@ function Post() {
         </div>
       </PostInfo>
 
-      <div id="editor" className="ql-editor" ref={divRef}></div>
+      <EditorContainer>
+        <div id="editor" className="ql-editor" ref={divRef}></div>
+      </EditorContainer>
+
+      <UserAndPostNumbers>
+        <User>
+          <UsernameAndImg
+            username="Andrew Campos"
+            imgUrl="https://media.licdn.com/dms/image/v2/D4D03AQECfeL929bGhQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1684103633996?e=1744243200&v=beta&t=UWg46XjeykG4ZwAGhHA9adJJteP9n9pCHtOlG5jKcGc"
+          />
+        </User>
+
+        <Icons>
+          {iconsData.map((icon) => (
+            <div>
+              {icon.icon}
+              <span>{icon.value}</span>
+            </div>
+          ))}
+        </Icons>
+      </UserAndPostNumbers>
     </PostContainer>
   );
 }
--- src/pages/Feed/Posts/Post/styles.ts
+++ src/pages/Feed/Posts/Post/styles.ts
@@ -9,7 +9,6 @@ export const PostContainer = styled.main`
   .ql-editor {
     height: max-content;
     padding: 0px;
-    background-color: gainsboro;
   }
 `;
 
@@ -28,5 +27,51 @@ export const PostInfo = styled.div`
     flex-grow: 1;
     padding-bottom: 8px;
     border-bottom: 2px solid rgba(0, 0, 0, 0.2);
+  }
+`;
+
+export const EditorContainer = styled.div`
+  border-bottom: 2px solid rgba(0, 0, 0, 0.1);
+  padding-bottom: 8px;
+`;
+
+export const UserAndPostNumbers = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 64px;
+  margin-top: 24px;
+`;
+
+export const User = styled.div`
+  display: flex;
+
+  img {
+    height: 48px;
+    width: 48px;
+  }
+`;
+
+export const Icons = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 24px;
+  font-size: 14px;
+  color: #777;
+
+  div {
+    display: flex;
+    align-items: center;
+  }
+
+  .ico {
+    color: ${({ theme }) => theme.paper.dark};
+  }
+
+  @media (max-width: 768px) {
+    font-size: 12px;
+    .ico {
+      height: 16px;
+      width: 16px;
+    }
   }
 `;
